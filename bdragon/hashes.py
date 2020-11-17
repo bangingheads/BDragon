667f5dd83ea83e4b41204e8feda924e95f587ce5
bangingheads <53184036+bangingheads@users.noreply.github.com>
2020-11-16 20:08:01 -0600

    Add Item Hashes

--- bdragon/hashes.py
+++ bdragon/hashes.py
@@ -11,6 +11,8 @@ def find_hashes():
     hashes = {}
     championfull = utils.load_json(os.path.join(
         settings.files, settings.patch['json'] + "/data/en_US/championFull.json"))
+    items = utils.load_json(os.path.join(
+        settings.files, settings.patch['json'] + "/data/en_US/item.json"))
 
     for champion in championfull['data']:
         for x in championfull['data'][champion]['spells']:
@@ -24,6 +26,13 @@ def find_hashes():
                 string = string.replace(" ", "")
                 hashes.update({
                     string: create_hash(string)
+                })
+
+    for item in items['data']:
+        if "datavalues" in items['data'][item]:
+            for y in items['data'][item]['datavalues']:
+                hashes.update({
+                    y: create_hash(y)
                 })
 
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "guessedhashes.txt"), "a+") as file_object:
@@ -48,7 +57,10 @@ def sort_hashes():
             hashes[value] = key
     hashes = dict(sorted(hashes.items()))
     os.remove(os.path.join(os.path.dirname(
-        os.path.realpath(__file__)), "trans/hashes.txt"))
+        os.path.realpath(__file__)), "hashes.txt"))
     with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "hashes.txt"), "a+") as f:
         for x in hashes:
             f.write(hashes[x] + " " + x + "\n")
+
+
+sort_hashes()
